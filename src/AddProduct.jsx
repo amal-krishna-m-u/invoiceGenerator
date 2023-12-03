8ac2c4e12e7340c45abbdd3b2b3e538009c5466c
amalkrsihna <amalkrsihna@ug.cusat.ac.in>
2023-12-03 14:48:05 +0530

    refactor(AddProduct): removed deadsnipets

--- src/AddProduct.jsx
+++ src/AddProduct.jsx
@@ -1,19 +1,27 @@
 // AddProduct.jsx
 import React, { useState } from "react";
 
+// AddProduct component to add a new product
 const AddProduct = ({ categories, onAddProduct }) => {
+  // State to track the new product inputs
   const [newProduct, setNewProduct] = useState({ name: "", price: "", category: "" });
 
+  // Function to handle adding a new product
   const handleAddProduct = () => {
+    // Check if name, price, and category are provided
     if (newProduct.name && newProduct.price && newProduct.category) {
+      // Call the onAddProduct prop to pass the new product
       onAddProduct(newProduct);
+      // Reset the newProduct state after adding
       setNewProduct({ name: "", price: "", category: "" });
     }
   };
 
+  // Render the component UI
   return (
     <div>
       <h2>Add Product</h2>
+      {/* Input for product name */}
       <input
         type="text"
         placeholder="Product Name"
@@ -21,6 +29,7 @@ const AddProduct = ({ categories, onAddProduct }) => {
         onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
       />
       <br />
+      {/* Input for product price */}
       <input
         type="number"
         placeholder="Product Price"
@@ -28,11 +37,13 @@ const AddProduct = ({ categories, onAddProduct }) => {
         onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
       />
       <br />
+      {/* Dropdown for selecting product category */}
       <select
         value={newProduct.category}
         onChange={(e) => setNewProduct({ ...newProduct, category: e.target.value })}
       >
         <option value="">Select Category</option>
+        {/* Map through categories and create options */}
         {categories.map((category) => (
           <option key={category.name} value={category.name}>
             {category.name}
@@ -40,9 +51,11 @@ const AddProduct = ({ categories, onAddProduct }) => {
         ))}
       </select>
       <br />
+      {/* Button to trigger adding the product */}
       <button onClick={handleAddProduct}>Add Product</button>
     </div>
   );
 };
 
+// Export the AddProduct component
 export default AddProduct;
