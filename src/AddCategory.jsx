fce5b83539280c4047f247960436ec1cbf75d3cc
amalkrsihna <amalkrsihna@ug.cusat.ac.in>
2023-12-03 14:47:05 +0530

    feat: add new feature

--- src/AddCategory.jsx
+++ src/AddCategory.jsx
@@ -1,19 +1,27 @@
 // AddCategory.jsx
 import React, { useState } from "react";
 
+// Functional component for adding a new category
 const AddCategory = ({ onAddCategory }) => {
+  // State to track the new category inputs
   const [newCategory, setNewCategory] = useState({ name: "", gst: "" });
 
+  // Function to handle adding a new category
   const handleAddCategory = () => {
+    // Check if both name and gst are provided
     if (newCategory.name && newCategory.gst) {
+      // Call the onAddCategory prop to pass the new category
       onAddCategory(newCategory);
+      // Reset the newCategory state after adding
       setNewCategory({ name: "", gst: "" });
     }
   };
 
+  // Render the component UI
   return (
     <div>
       <h2>Add Category</h2>
+      {/* Input for category name */}
       <input
         type="text"
         placeholder="Category Name"
@@ -21,6 +29,7 @@ const AddCategory = ({ onAddCategory }) => {
         onChange={(e) => setNewCategory({ ...newCategory, name: e.target.value })}
       />
       <br />
+      {/* Input for category GST */}
       <input
         type="text"
         placeholder="Category GST (%)"
@@ -28,9 +37,11 @@ const AddCategory = ({ onAddCategory }) => {
         onChange={(e) => setNewCategory({ ...newCategory, gst: e.target.value })}
       />
       <br />
+      {/* Button to trigger adding the category */}
       <button onClick={handleAddCategory}>Add Category</button>
     </div>
   );
 };
 
+// Export the AddCategory component
 export default AddCategory;
